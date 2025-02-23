7651fe98abc7e4bdfb8f79567ea2c31dbc316a72
leonardosantos-hdsd <153404275+leonardosantos-hdsd@users.noreply.github.com>
2025-02-23 20:20:35 -0300

    Add files via upload
    Esse exercício consiste justamente em criar um programa em Java que imprima “Hello World!“ no console :)
    
    Comentário para chamada direta do “Hello World!“ e rodando com uma main chamado um método público na classe HelloWorld chamado printMessage contendo a impressão de “Hello World!“ na tela.

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.helloworld;
+
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        System.out.println("Hello world! Arquivo Main");
+//    }
+//}
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printMessage();
+    }
+}
