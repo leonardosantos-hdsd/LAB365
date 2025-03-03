f3fc03108f53e2cd7b309196093111e59ad8be6f
leonardosantos-hdsd <153404275+leonardosantos-hdsd@users.noreply.github.com>
2025-03-02 21:15:11 -0300

    Update S3_Main.java

--- S3_Main.java
+++ S3_Main.java
@@ -1,12 +1,13 @@
 package com.semana03;
 
-public class Main_s3 {
+public class S3_Main {
     public static void main(String[] args) {
+
         // Criação de um array de inteiros para teste
         int[] valores = {10, 20, 30, 40, 50};
 
         // Instanciar a classe de cálculo
-        ArrayCalculator calculadora = new ArrayCalculator();
+        S3_ArrayCalculator calculadora = new S3_ArrayCalculator();
 
         // Calcular a soma dos elementos
         int resultado = calculadora.somarElementos(valores);
